bddac18749be6f61e060c8bf37bec8c90879e478
vobilinovic <veronica.obilinovic@dap.cl>
2026-02-26 09:03:46 -0300

    cambios

--- portafolio/src/pages/Inicio.jsx
+++ portafolio/src/pages/Inicio.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import Navbar from "./components/Navbar";
-import Presentacion from './components/Presentacion';
-import Formacion from './components/Formacion';
-import Experiencia from './components/Experiencia';
-import Proyectos from './components/Proyectos';
+import Navbar from "../components/Navbar";
+import Presentacion from '../components/Presentacion';
+import Formacion from '../components/Formacion';
+import Experiencia from '../components/Experiencia';
+import Proyectos from '../components/Proyectos';
 
 function App() {
   return (
---
+++ portafolio/src/pages/ProyectoDetalle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProyectoDetalle() {
+  return (
+    <div>
+      <h1>Detalle del Proyecto</h1>
+    </div>
+  );
+}
+
+export default ProyectoDetalle;
